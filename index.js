bd3d7d7ec32f73e4cc5d0a34197e66204052a21b
avalladaresm <avalladaresm@unitec.edu>
2020-07-10 05:02:15 -0600

    added cors config, needs understanding of headers setup

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var path = require('path')
 var http = require('http')
 var oas3Tools = require('oas3-tools')
 const bodyParser = require('body-parser')
+var cors = require('cors')
 var serverPort = 8080
 
 // swaggerRouter configuration
@@ -19,6 +20,21 @@ var app = expressAppConfig.getApp()
 app.set('view-engine', 'pug')
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000', 'http://192.168.1.100:8000');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', false);
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Content-Length,Server,Date,Access-Control-Allow-Credentials,Access-Control-Allow-Methods,Access-Control-Allow-Origin,Accept,Origin,X-Requested-With');
+  // Pass to next layer of middleware
+  next();
+});
+app.options('*', cors());
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
