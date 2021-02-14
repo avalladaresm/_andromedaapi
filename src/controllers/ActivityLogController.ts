6249b5bca059f5443d170e10d01566cad799ecb3
avalladaresm <avalladaresm@unitec.edu>
2021-02-14 13:28:04 -0600

    added controller to create other activity logs like visited page

--- src/controllers/ActivityLogController.ts
+++ src/controllers/ActivityLogController.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, UseBefore } from '@tsed/common';
+import { BodyParams, Controller, Get, Post, UseBefore } from '@tsed/common';
 import { ContentType } from '@tsed/schema';
 import { AuthorizeRequest } from '../middlewares/AuthorizeRequest';
-import { ActivityLogResult } from '../models/ActivityLog';
+import { ActivityLogCreate, ActivityLogResult } from '../models/ActivityLog';
 import { ActivityLogService } from '../services/ActivityLogService';
+import { appendActivityLogDescription } from '../utils';
 
 @Controller('/activitylog')
 @UseBefore(AuthorizeRequest)
@@ -15,6 +16,18 @@ export class ActivityLogController {
     try {
       const activityLogs: ActivityLogResult[] = await this.activityLogService.getActivityLogs()
       return activityLogs
+    }
+    catch (e) {
+      throw e
+    }
+  }
+
+  @Get('/createActivityLog')
+  @ContentType('application/json')
+  async createActivityLog(@BodyParams('data') data: ActivityLogCreate): Promise<void> {
+    try {
+      const appendedData = appendActivityLogDescription(data)
+      await this.activityLogService.createActivityLog(appendedData)
     }
     catch (e) {
       throw e
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,19 @@
+import { ActivityLogCreate } from "../models/ActivityLog"
 var bcrypt = require('bcrypt')
 
 export const hashPassword = async (password: string): Promise<string> => {
-    const hash = await bcrypt.hash(password, 10)
-    return hash
+  const hash = await bcrypt.hash(password, 10)
+  return hash
+}
+
+export const appendActivityLogDescription = (data: ActivityLogCreate) => {
+  switch (data.typeId) {
+    case 8:
+      data.description = `${data.username} visited route ${data.data}`
+      break
+    default:
+      return data
+  }
+  
+  return data
 }
